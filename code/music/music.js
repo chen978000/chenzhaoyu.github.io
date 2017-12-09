174d047a52477237e6ae1f4a1fa9eeaad0681727
chen978000 <33387897+chen978000@users.noreply.github.com>
2017-12-09 18:37:45 +0800

    Update music.js

--- code/music/music.js
+++ code/music/music.js
@@ -186,7 +186,7 @@ function randomMode(){
 function showList(hideBtn){
 	var musicList = document.querySelector(".music_list");
 	musicList.style.top = "45%";
-	musicList.style.height = "400px";
+	musicList.style.height = "360px";
 	hideBtn.style.display = "block";
 	musicList.onscroll = function(){
 		hideBtn.style.top = musicList.scrollTop+"px";	
@@ -196,8 +196,8 @@ function showList(hideBtn){
 //隐藏歌单
 function hideList(hideBtn){
 	var musicList = document.querySelector(".music_list");
-	musicList.style.top = "95%";
-	musicList.style.height = "30px";
+	musicList.style.top = "96%";
+	musicList.style.height = "25px";
 	hideBtn.style.display = "none";
 	musicList.scrollTop = "0";
 }
@@ -205,7 +205,7 @@ function hideList(hideBtn){
 function getList(){
 	var listUl = document.querySelector(".music_list_ul");
 	for(var j=0; j<List.length;j++){
-		var content = document.createTextNode(List[j].name+"——"+List[j].artist);
+		var content = document.createTextNode(List[j].name+"——"+List[j].artist+"——"+List[j].album);
 		var newSpan = document.createElement("span");
 		newSpan.className = "listMusic";
 		var newLi = document.createElement("li");
@@ -247,131 +247,234 @@ function setTime(x){
 	return m+":"+s;
 }
 
-//歌单（更新于20171207）
+//歌单（更新于20171209）
 var List =[ 
 	{"name":"成都", 
 	"id":"436514312", 
 	"img":"http://p1.music.126.net/34YW1QtKxJ_3YnX9ZzKhzw==/2946691234868155.jpg", 
-	"artist":"赵雷"
+	"artist":"赵雷",
+	"album":"<成都>"
 	},
 	{"name":"童话镇", 
 	"id":"432506345", 
 	"img":"http://p1.music.126.net/tfa811GLreJI_S0h9epqRA==/3394192426154346.jpg", 
-	"artist":"陈一发儿"
+	"artist":"陈一发儿",
+	"album":"<童话镇>"
 	},
 	{"name":"理想三旬", 
 	"id":"31445772", 
 	"img":"http://p1.music.126.net/_msXsY9oMY4MSG3OTN0U1A==/109951162849778878.jpg", 
-	"artist":"陈鸿宇"
+	"artist":"陈鸿宇",
+	"album":"<浓烟下的诗歌电台>"
 	},
 	{"name":"平凡之路", 
 	"id":"28815250", 
 	"img":"http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg", 
-	"artist":"朴树"
+	"artist":"朴树",
+	"album":"<猎户星座>"
 	},
 	{"name":"追梦赤子心", 
 	"id":"355992", 
 	"img":"http://p1.music.126.net/XDncptlBJ4_LN3hLBx-8aw==/19061133579343591.jpg", 
-	"artist":"GALA"
+	"artist":"GALA",
+	"album":"<动画《那年那兔那些事儿》第一季片尾曲>"
 	},
 	{"name":"南方姑娘", 
 	"id":"202373", 
 	"img":"http://p1.music.126.net/wldFtES1Cjnbqr5bjlqQbg==/18876415625841069.jpg", 
-	"artist":"赵雷"
+	"artist":"赵雷",
+	"album":"<赵小雷>"
 	},
 	{"name":"晴天", 
 	"id":"186016", 
 	"img":"http://p1.music.126.net/yjVbsgfNeF2h7fIvnxuZDQ==/18894007811887644.jpg", 
-	"artist":"周杰伦"
+	"artist":"周杰伦",
+	"album":"<叶惠美>"
 	},
 	{"name":"理想", 
 	"id":"29567189", 
 	"img":"http://p1.music.126.net/PJNV84mjt_mDXEkxtjzB4w==/18957779486268444.jpg", 
-	"artist":"赵雷"
+	"artist":"赵雷",
+	"album":"<吉姆餐厅>"
 	},
 	{"name":"秋酿", 
 	"id":"30635613", 
 	"img":"http://p1.music.126.net/XSCmc9ZbWcpeizDya2g6JQ==/18598239185572641.jpg", 
-	"artist":"房东的猫"
+	"artist":"房东的猫",
+	"album":"<房东的猫>"
 	},
 	{"name":"安和桥", 
 	"id":"27646205", 
 	"img":"http://p1.music.126.net/mPlr0GoQU2Wl_aZzIgIJ6A==/1984618488161733.jpg", 
-	"artist":"宋冬野"
+	"artist":"宋冬野",
+	"album":"<安和桥北>"
 	},
 	{"name":"光阴的故事", 
 	"id":"26361010", 
 	"img":"http://p1.music.126.net/3K6W80SyZ0ZIy3btLRlZMw==/7820826208648668.jpg", 
-	"artist":"黄晓明 / 邓超 / 佟大为"
+	"artist":"黄晓明 / 邓超 / 佟大为",
+	"album":"<电影《中国合伙人》主题曲>"
 	},
 	{"name":"阴天", 
 	"id":"276936", 
 	"img":"http://p1.music.126.net/DjyL8sXf9iU0Cik4hzs4RQ==/109951162783381.jpg", 
-	"artist":"莫文蔚"
+	"artist":"莫文蔚",
+	"album":"<就 i Karen 莫文蔚精选>"
 	},
 	{"name":"最重要的决定", 
 	"id":"229161", 
 	"img":"http://p1.music.126.net/N9fWHLChdjpcGa1QbCbhDg==/19014954090943253.jpg", 
-	"artist":"范玮琪"
+	"artist":"范玮琪",
+	"album":"<Love and FanFan>"
 	},
 	{"name":"有点甜", 
 	"id":"165340", 
 	"img":"http://p1.music.126.net/2rMIJCU94hXW4pmxayvhPQ==/96757023249983.jpg", 
-	"artist":"汪苏泷 / By2"
+	"artist":"汪苏泷 / By2",
+	"album":"<万有引力>"
 	},
 	{"name":"Honey", 
 	"id":"297829", 
 	"img":"http://p1.music.126.net/YiTOLZAw20itRSFsHHBaPw==/18657612813739192.jpg", 
-	"artist":"王心凌"
+	"artist":"王心凌",
+	"album":"<Honey>"
 	},
 	{"name":"风继续吹", 
 	"id":"26620762", 
 	"img":"http://p1.music.126.net/AtMpiqgluHefLF6nD_Pcug==/4417837720418086.jpg", 
-	"artist":"张国荣"
+	"artist":"张国荣",
+	"album":"<最红>"
 	},
 	{"name":"一笑倾城", 
 	"id":"426852063", 
 	"img":"http://p1.music.126.net/XtQbs_V1OAvtZo9tmJFAJA==/1369991503018202.jpg", 
-	"artist":"汪苏泷"
+	"artist":"汪苏泷",
+	"album":"<电视剧《微微一笑很倾城》片头曲>"
 	},
 	{"name":"文科生的一个下午", 
 	"id":"5265943", 
 	"img":"http://p1.music.126.net/4FY52KxHJezy0lbpEugllw==/102254581400467.jpg", 
-	"artist":"赵节"
+	"artist":"赵节",
+	"album":"<我的最爱—校园歌谣十周年>"
 	},
 	{"name":"那些花儿(吉他版)", 
 	"id":"139392", 
 	"img":"http://p1.music.126.net/QYI8P7bfmwUKxH3Kll95LQ==/18878614649179588.jpg", 
-	"artist":"朴树"
+	"artist":"朴树",
+	"album":"<我去2000年>"
 	},
 	{"name":"一腔诗意喂了狗", 
 	"id":"460542191", 
 	"img":"http://p1.music.126.net/d95pQeIaep9IKy3gI0fKXw==/109951162862111882.jpg", 
-	"artist":"花粥"
+	"artist":"花粥",
+	"album":"<2017（三）>"
 	},
 	{"name":"想把我唱给你听", 
 	"id":"297612", 
 	"img":"http://p1.music.126.net/EmcMtmLwXZaOfCG1454oyQ==/73667279078627.jpg", 
-	"artist":"王筝 / 老狼 / 小柯 / 曹方"
+	"artist":"王筝 / 老狼 / 小柯 / 曹方",
+	"album":"<我们都是好孩子>"
 	},
 	{"name":"干杯，朋友", 
 	"id":"293721", 
 	"img":"http://p1.music.126.net/1Z7Rvewx5xsdBl24doz5fA==/64871186051046.jpg", 
-	"artist":"田震"
+	"artist":"田震",
+	"album":"<干杯，田震>"
 	},
 	{"name":"晴天娃娃", 
 	"id":"185846", 
 	"img":"http://p1.music.126.net/UAgXkK9Buq7NcFJ2fy0KhA==/84662395351539.jpg", 
-	"artist":"江语晨"
+	"artist":"江语晨",
+	"album":"<电影《不能说的秘密》原声带>"
 	},
 	{"name":"心愿", 
 	"id":"379785", 
 	"img":"http://p1.music.126.net/-QgrpnBhavRcLtaBaad5Lw==/52776558150270.jpg", 
-	"artist":"四个女生"
+	"artist":"四个女生",
+	"album":"<心愿>"
 	},
 	{"name":"蓝莲花", 
 	"id":"168091", 
 	"img":"http://p1.music.126.net/C6txAWMGlVmAcAD37RVutQ==/122045790684028.jpg", 
-	"artist":"许巍"
+	"artist":"许巍",
+	"album":"<时光.漫步>"
+	},
+	{"name":"可惜没如果", 
+	"id":"29814898", 
+	"img":"http://p1.music.126.net/X0EDfXzxMQJiQ-71JFGdZw==/3238061746556733.jpg", 
+	"artist":"林俊杰",
+	"album":"<新地球>"
+	},
+	{"name":"曾经的你", 
+	"id":"167975", 
+	"img":"http://p1.music.126.net/ievBOR3ZkrDpPoMKrcaSog==/107752139534188.jpg", 
+	"artist":"许巍",
+	"album":"<每一刻都是崭新的>"
+	},
+	{"name":"外婆", 
+	"id":"186003", 
+	"img":"http://p1.music.126.net/P1goeQ7SoxEkFsb4ZDijMw==/7746059418324672.jpg", 
+	"artist":"周杰伦",
+	"album":"<七里香>"
+	},
+	{"name":"我也很想他", 
+	"id":"287280", 
+	"img":"http://p1.music.126.net/pwInkIYzovf72Mqm5T0Kzg==/768558627827557.jpg", 
+	"artist":"孙燕姿",
+	"album":"<Stefanie>"
+	},
+	{"name":"Kiss Me", 
+	"id":"28188178", 
+	"img":"http://p1.music.126.net/kjFVkzzt6BzB9EXr0qB1zA==/2576155743910424.jpg", 
+	"artist":"Charlie Puth",
+	"album":"<Ego>"
+	},
+	{"name":"千千阙歌", 
+	"id":"212233", 
+	"img":"http://p1.music.126.net/tzmGFZ0-DPOulXS97H5rmA==/18712588395102549.jpg", 
+	"artist":"陈慧娴",
+	"album":"<千千阙歌>"
+	},
+	{"name":"夜空中最亮的星", 
+	"id":"25706282", 
+	"img":"http://p1.music.126.net/d541jx7SqoDzVkenhmtg6g==/19193074974743620.jpg", 
+	"artist":"逃跑计划",
+	"album":"<世界>"
+	},
+	{"name":"阳光宅男", 
+	"id":"185813", 
+	"img":"http://p1.music.126.net/8LmuJ7_7MMbubyuF1d0BhA==/19082024300336034.jpg", 
+	"artist":"周杰伦",
+	"album":"<我很忙>"
+	},
+	{"name":"我在人民广场吃炸鸡", 
+	"id":"27501817", 
+	"img":"http://p1.music.126.net/oBXMDspbznSG3XdC0Ck9EA==/5636096604050877.jpg", 
+	"artist":"阿肆",
+	"album":"<预谋邂逅>"
+	},
+	{"name":"我的主题曲", 
+	"id":"247945", 
+	"img":"http://p1.music.126.net/oAUF0c9gCkcVQvrSvvmu2g==/556352883666613.jpg", 
+	"artist":"江语晨",
+	"album":"<晴天娃娃>"
+	},
+	{"name":"Naked", 
+	"id":"520012673", 
+	"img":"http://p1.music.126.net/HLFD7XjxSAgDbaoN1ZhQ0Q==/18220007184446832.jpg", 
+	"artist":"James Arthur",
+	"album":"<Naked>"
+	},
+	{"name":"独家记忆", 
+	"id":"63650", 
+	"img":"http://p1.music.126.net/D_e8zPgwkKCk1uSF-HSgbw==/18806046883327256.jpg", 
+	"artist":"陈小春",
+	"album":"<独家记忆 (Hong Kong Version)>"
+	},
+	{"name":"星语心愿", 
+	"id":"330706", 
+	"img":"http://p1.music.126.net/zRd1sSA2YIQfQUHR9aLFRQ==/30786325592352.jpg", 
+	"artist":"张柏芝",
+	"album":"<电影《星愿》插曲>"
 	}
 ];
