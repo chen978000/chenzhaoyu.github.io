691ca6b7d2304fd05dece4980651b8eeb2745463
chen978000 <33387897+chen978000@users.noreply.github.com>
2017-12-14 18:31:42 +0800

    Add files via upload

---
+++ code/music/xml.php
@@ -0,0 +1,16 @@
+<?php
+    //获取输入的内容
+    $name = $_GET["name"];
+     //设置时区  
+     date_default_timezone_set('Asia/Shanghai');  
+     //按指定格式输出日期  
+     $date=date('Y-m-d H:i');  
+     //输出字符串拼接
+     $message = $date.":".$name."\r\n";
+     
+     @$fp = fopen("musci.txt", "ab");
+     flock($fp,LOCK_EX);
+     fwrite($fp,$message);
+     flock($fp,LOCK_UN);
+     fclose($fp);
+?>
