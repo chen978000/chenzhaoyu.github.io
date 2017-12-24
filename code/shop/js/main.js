d42223148714a657cfbaffed6a3c59d126b6aa60
chen978000 <33387897+chen978000@users.noreply.github.com>
2017-12-24 22:20:33 +0800

    Add files via upload

---
+++ code/shop/js/main.js
@@ -0,0 +1,165 @@
+
+window.onload = function () {
+    //加载推荐
+    getRec();
+    //顶部
+    // headChange();
+    Banner();
+}
+//创建推荐
+function getRec() {
+    var oRecUl = document.querySelector(".recUl");
+    for (var i = 0; i < recList.length; i = i + 2) {
+        var eImg1 = document.createElement("img");
+        var eImg2 = document.createElement("img");
+        eImg1.src = recList[i].src;
+        eImg2.src = recList[i + 1].src;
+        var eP1 = document.createElement("p");
+        var eP2 = document.createElement("p");
+        eP1.innerHTML = recList[i].desc;
+        eP2.innerHTML = recList[i + 1].desc;
+        eP1.className = "desc";
+        eP2.className = "desc";
+        var eP11 = document.createElement("p");
+        eP11.innerHTML = "请登陆查看价格";
+        eP11.className = "Hprice";
+        var eP21 = document.createElement("p");
+        eP21.innerHTML = "请登陆查看价格";
+        eP21.className = "Hprice";
+        var eA1 = document.createElement("a");
+        var eA2 = document.createElement("a");
+        eA1.href = recList[i].href;
+        eA2.href = recList[i + 1].href;
+        var eLi = document.createElement("li");
+        eA1.appendChild(eImg1);
+        eA1.appendChild(eP1);
+        eA1.appendChild(eP11);
+        eA2.appendChild(eImg2);
+        eA2.appendChild(eP2);
+        eA2.appendChild(eP21);
+        eLi.appendChild(eA1);
+        eLi.appendChild(eA2);
+        oRecUl.appendChild(eLi);
+    }
+}
+
+//顶部
+function headChange() {
+    var header = document.querySelector(".header_box");
+    var banner = document.querySelector(".banner");
+    var height = banner.offsetHeight;
+    window.onscroll = function () {
+        var opacity = 0;
+        var top = document.documentElement.scrollTop;
+        if (top > height) {
+            opacity = 1;
+        } else {
+            opacity = (top / height);
+        }
+        header.style.background = "rgba(156,64,25," + opacity + ")";
+    }
+}
+
+//轮播图
+function Banner() {
+    var banner = document.querySelector(".banner");
+    var width = banner.offsetWidth;
+    var banUl = document.querySelector(".bannerUl");
+    var pointUl = banner.querySelector("ul:last-child");
+    var points = pointUl.querySelectorAll("li");
+    var index = 1;
+    function imgMove(){
+        index++;
+        addTransition(banUl);
+        setTranslateX(banUl, (-index*width));
+    }
+    var timer = setInterval(imgMove, 2000);
+    banUl.addEventListener("transitionend", function(){
+        if(index>=7){
+            index=1;
+            removeTransition(banUl);
+            setTranslateX(banUl,(-index*width));
+        }else if(index<=0){
+            index=6;
+            removeTransition(banUl);
+            setTranslateX(banUl,(-index*width));      
+        }
+        //点的移动
+        for(var i=0;i<points.length;i++){
+            points[i].classList.remove("now");
+        }
+        points[index-1].classList.add("now");
+    });
+    banUl.addEventListener("mouseover", function(){
+        clearInterval(timer);
+    });
+    banUl.addEventListener("mouseout", function(){
+        timer = setInterval(imgMove, 2000);
+    });
+    //点击点
+    for(var i=0;i<points.length;i++){
+        points[i].index = i;
+        points[i].addEventListener("click",function(){
+            clearInterval(timer);
+            index = this.index+1;
+            addTransition(banUl);
+            setTranslateX(banUl, (-index*width));
+            points[i].addEventListener("mouseout",function(){
+                timer = setInterval(imgMove, 2000);
+            });
+        });
+
+    }
+
+    //移动端滑动
+    var startX = 0;
+    var distance = 0;
+    var translateX = 0;
+    var isMove = false;
+    banUl.addEventListener("touchstart", function(event){
+        startX = event.touches[0].clientX;
+        clearInterval(timer);
+    });
+    banUl.addEventListener("touchmove", function(event){
+        var moveX = event.touches[0].clientX;
+        distance = moveX-startX;
+        translateX = -index*width+distance;
+        removeTransition(banUl);
+        setTranslateX(banUl, translateX);
+        isMove = true;
+    });
+    banUl.addEventListener("touchend",function(event){
+        if(isMove){
+            if(Math.abs(distance)<width/3){
+                addTransition(banUl);
+                setTranslateX(banUl, (translateX-distance));  
+            }else if(distance<0){
+                index++;
+                addTransition(banUl);
+                setTranslateX(banUl,(-index*width));
+            }else if(distance>0){
+                index--;
+                addTransition(banUl);
+                setTranslateX(banUl,(-index*width));
+            }
+        }
+        timer = setInterval(imgMove, 2000);
+        isMove = false;
+    });
+}
+
+//加过渡
+function addTransition(obj) {
+    obj.style.transition = "all 0.5s";
+    obj.style.webkitTransition = "all 0.5s";
+}
+//去过渡
+function removeTransition(obj) {
+    obj.style.transition = "none";
+    obj.style.webkitTransition = "none";
+}
+//设置X位移
+function setTranslateX(obj, x) {
+    obj.style.transform = "translateX(" + x + "px)";
+    obj.style.webkitTransform = "translateX(" + x + "px)";
+}
---
+++ code/shop/js/recList.js
@@ -0,0 +1,162 @@
+var recList = [
+    {"src":"./img/rec1.jpg",
+    "desc":"安井袋装蛋饺165g/袋 60袋/箱",
+    "href":"#"
+    },
+    {"src":"./img/rec2.png",
+    "desc":"大象去皮脖60/80 10kg",
+    "href":"#"
+    },
+    {"src":"./img/rec3.jpg",
+    "desc":"安井金麦流沙包[300g/袋 12袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec4.jpg",
+    "desc":"巴西4430全腿 15kg",
+    "href":"#"
+    },
+    {"src":"./img/rec5.png",
+    "desc":"宏泰白条鸭 1.35kg/只 10只/箱",
+    "href":"#"
+    },
+    {"src":"./img/rec6.png",
+    "desc":"智利0602前肘 [20kg/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec7.png",
+    "desc":"乾信袋装排腿300/350 [9.6kg/板丨2板/袋]",
+    "href":"#"
+    },
+    {"src":"./img/rec8.png",
+    "desc":"吉康鱼味丸 [2.5kg/袋丨4袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec9.png",
+    "desc":"安井散装玲珑鱼果 [2.5kg/袋丨4袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec10.jpg",
+    "desc":"正大荠菜猪肉蒸饺 [400g/袋丨18袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec11.jpg",
+    "desc":"正大酸菜肉丁面 [330g/盒丨16盒/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec12.jpg",
+    "desc":"正大酸菜肉丁饭 [400g/盒丨12盒/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec13.png",
+    "desc":"正大黄金蛋炒饭 [330g/盒丨16盒/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec14.jpg",
+    "desc":"正大黑椒牛肉面 [330g/盒丨16盒/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec15.jpg",
+    "desc":"正大胡萝卜粉丝包 [510g/袋丨12袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec16.png",
+    "desc":"正大台烤一号（黑胡椒风味）[0.4kg/袋丨24袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec17.jpg",
+    "desc":"正大台烤一号（原味）[0.4kg/袋丨24袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec18.png",
+    "desc":"安井切片原味千夜豆腐 [2.5kg/袋丨4袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec19.png",
+    "desc":"澎澎撒尿肉丸 [2.5kg/袋丨4袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec20.png",
+    "desc":"吉康台肠70g [2.1kg/袋丨6袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec21.png",
+    "desc":"吉康夹心蟹排 [2.5kg/袋丨4袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec22.png",
+    "desc":"六和爪骨 [1kg/袋丨12袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec23.png",
+    "desc":"安井夹心仿蟹排 [2.5kg/袋丨4袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec24.jpg",
+    "desc":"春雪爆汁鸡米花(原味) [1kg/袋丨10袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec25.png",
+    "desc":"正大鸡肉洋葱圈 [800g/袋丨10袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec26.jpg",
+    "desc":"鹭明雪花培根 [1.5kg/袋丨8袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec27.png",
+    "desc":"正大菌菇三鲜蒸饺 [400g/袋丨18袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec28.png",
+    "desc":"耘垦边腿 [7.2kg/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec29.png",
+    "desc":"宇飞锁骨1/2 [10kg/袋]",
+    "href":"#"
+    },
+    {"src":"./img/rec30.png",
+    "desc":"耘垦边腿 [7.2kg/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec31.jpg",
+    "desc":"正大美味鸡块(黑椒) [1kg/袋丨10袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec32.jpg",
+    "desc":"思念猪肉香菇馄饨 [220g/袋丨32袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec33.png",
+    "desc":"思念三鲜馄饨 [220g/袋丨32袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec34.png",
+    "desc":"安井香芋地瓜丸 [420g/袋丨10袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec35.png",
+    "desc":"吉康黄金小鱼豆腐 [2.5kg/袋丨4袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec36.png",
+    "desc":"诸城吉康台湾烤肠38g 300支 [1.9kg/袋丨6袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec37.png",
+    "desc":"吉康白桂花肠 [2.5kg/袋丨4袋/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec38.png",
+    "desc":"六和鸭排腿 [10kg/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec39.png",
+    "desc":"六和单冻大胸（不修边）[10kg/箱]",
+    "href":"#"
+    },
+    {"src":"./img/rec40.png",
+    "desc":"宏发100/120琵琶腿 [10kg/箱]（90个左右）",
+    "href":"#"
+    }
+]
